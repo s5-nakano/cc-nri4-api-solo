2ea6e92c68183635d850b90431b33253d817d075
s5-nakano <sawayakagogogo@gmail.com>
2023-11-19 18:26:08 +0900

    fix-2

--- knexfile.js
+++ knexfile.js
@@ -12,9 +12,15 @@ module.exports = {
     // host: process.env.DB_HOST || "127.0.0.1",
     host: process.env.DB_HOST || "postgres",
     port: process.env.DB_PORT || 5432,
+    /** ローカル環境ではこれ
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    */
+    database: process.env.DB_NAME || "todo_apps",
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+
   },
   migrations: {
     directory: "./db/migrations",

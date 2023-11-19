1959e6d293e814f20df7a2ed99698b60a2979418
s5-nakano <sawayakagogogo@gmail.com>
2023-11-19 18:41:17 +0900

    fix-4

--- knexfile.js
+++ knexfile.js
@@ -9,8 +9,8 @@ module.exports = {
   client: "pg",
   connection: process.env.DB_URL || {
     // ローカル環境では"127.0.0.1" or "localhost"
-    // host: process.env.DB_HOST || "127.0.0.1",
-    host: process.env.DB_HOST || "postgres",
+    host: process.env.DB_HOST || "127.0.0.1",
+    // host: process.env.DB_HOST || "postgres",
     port: process.env.DB_PORT || 5432,
     /** ローカル環境ではこれ
     database: process.env.DB_NAME,
